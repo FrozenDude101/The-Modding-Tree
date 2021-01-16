b9611c7b4b18dc1c4fb289925d3cc0d70fbe5676
FrozenDude101 <FrozenDude101@outlook.com>
2021-01-16 01:01:20 +0000

    v0.1.1.1 - Minor graphical tweaks.
    Added an outline to big nodes.
    Reduced the opacity of all node text.
    All displayed points are now floored to hide rounding errors.

--- js/mod.js
+++ js/mod.js
@@ -12,12 +12,18 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.1.1",
+	num: "0.1.1.1",
 	name: "Full Spectrum",
 }
 
 let changelog = `
 <h1>Changelog</h1><br>
+<br>
+<h3>v0.1.1.1</h3><br>
+<br>
+Added an outline to big nodes.<br>
+Reduced the opacity of all node text.<br>
+All displayed points are now floored to hide rounding errors.<br>
 <br>
 <h2><u>v0.1.1</u></h2><br>
 <br>
