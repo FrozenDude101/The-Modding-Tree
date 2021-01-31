566a2de5cac4338f882572aef03627d0f2e49db1
FrozenDude101 <FrozenDude101@outlook.com>
2021-01-31 02:12:58 +0000

    File shave and mod.js changes.

--- js/mod.js
+++ js/mod.js
@@ -1,31 +1,28 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "Treetorial",
+	id: "FD101/Treetorial",
+	author: "FrozenDude101",
+	pointsName: "completed modules",
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	initialStartPoints: new Decimal (0), // Used for hard resets and new players
 	
 	offlineLimit: 1,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1.1",
+	name: "Javascript",
 }
 
-let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+let changelog = ``;
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = ``;
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
-var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
+var doNotCallTheseFunctionsEveryTick = [];
 
 function getStartPoints(){
     return new Decimal(modInfo.initialStartPoints)
@@ -33,7 +30,7 @@ function getStartPoints(){
 
 // Determines if it should show points/sec
 function canGenPoints(){
-	return true
+	return false;
 }
 
 // Calculate points/sec!
@@ -46,8 +43,10 @@ function getPointGen() {
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
-function addedPlayerData() { return {
-}}
+function addedPlayerData() {
+	return {
+	};
+}
 
 // Display extra things at the top of the page
 var displayThings = [
