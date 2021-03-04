dd9d8e147d1607a02b9584babc78c5b332711e8d
FrozenDude101 <FrozenDude101@outlook.com>
2021-03-04 16:22:16 +0000

    Removed forced reset.

--- js/mod.js
+++ js/mod.js
@@ -75,5 +75,4 @@ function maxTickLength() {
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
 // you can cap their current resources with this.
 function fixOldSave(oldVersion) {
-	hardReset();
 }
